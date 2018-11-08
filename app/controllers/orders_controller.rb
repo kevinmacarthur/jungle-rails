8074dac59ec1baf04f5658325a82b401804a1531
kevinmacarthur <kmacarthur16@gmail.com>
2018-11-08 17:53:18 +0000

    added sending email to order email when order complete

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
 
     if order.valid?
       empty_cart!
+      OrderMailer.sample_email(order).deliver_now
       redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
@@ -55,5 +56,6 @@ class OrdersController < ApplicationController
     order.save!
     order
   end
+
 
 end
