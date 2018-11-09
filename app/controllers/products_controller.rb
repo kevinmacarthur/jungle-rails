e604d5ec2d263588d41c0b9283bf44781d25e6dd
kevinmacarthur <kmacarthur16@gmail.com>
2018-11-09 20:24:51 +0000

    added delete ability for reviews by the current logged in user, organized reviews by most recent at the top

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find params[:id]
     @review = Review.new
-
+    @current_user = current_user
   end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_review, only: [:destroy]
   before_action :set_product,
   # GET /reviews
   # GET /reviews.json
@@ -37,28 +37,11 @@ class ReviewsController < ApplicationController
       end
   end
 
-  # PATCH/PUT /reviews/1
-  # PATCH/PUT /reviews/1.json
-  def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
-      else
-        format.html { render :edit }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /reviews/1
   # DELETE /reviews/1.json
   def destroy
     @review.destroy
-    respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to product_path(@product)
   end
 
   private
@@ -70,7 +53,6 @@ class ReviewsController < ApplicationController
     def set_product
       @product = Product.find_by(id: params[:product_id])
     end
-
 
 
     # Never trust parameters from the scary internet, only allow the white list through.
